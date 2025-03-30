c1fa9c81d0cb301df4a0962b4d5f9df0768b257c
DanielaTrentin <devdanitrentin@gmail.com>
2025-03-30 18:23:11 -0300

    Placing pieces on the board

---
+++ src/BoardGame/King.java
@@ -0,0 +1,13 @@
+package BoardGame;
+
+public class King extends ChessPiece {
+
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
